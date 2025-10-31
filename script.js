3bd0b9114f272cfa139314e7232121a9382c8aa7
KomaliReddyVavilala <161595708+KomaliReddyVavilala@users.noreply.github.com>
2025-10-31 08:48:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,29 @@
-// complete this js code
 function Person(name, age) {
-	this.name = name;
-	this.age = age;
+  this.name = name;
+  this.age = age;
 }
+
 Person.prototype.greet = function () {
-	console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
-}
+  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+};
 
 function Employee(name, age, jobTitle) {
-	Person.call(this,name,age);
-	this.jobTitle = jobTitle;
+  // call Person constructor correctly
+  Person.call(this, name, age);
+  this.jobTitle = jobTitle;
 }
 
+// inherit from Person prototype
 Employee.prototype = Object.create(Person.prototype);
 
+// fix constructor reference
 Employee.prototype.constructor = Employee;
 
-Employee.prototype.jobGreet = function(){
-	console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
-}
+Employee.prototype.jobGreet = function () {
+  console.log(
+    `Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`
+  );
+};
 
 // Do not change code below this line
 window.Person = Person;
